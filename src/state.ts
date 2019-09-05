98c04e6674375a1ad1275b59c342657f1313cea9
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-09-05 19:15:23 +0200

    Simplify state check

--- src/state.ts
+++ src/state.ts
@@ -4,12 +4,15 @@ import * as constants from './constants';
 export function isActive() {
     const configuration = vscode.workspace.getConfiguration();
 
-    const formatOnSave = !!configuration.inspect(constants.configurationKeys.formatOnSave)!.globalValue;
-
     const codeActionsOnSave: any | undefined = configuration.inspect(constants.configurationKeys.codeActionsOnSave)!.globalValue;
+    if (!codeActionsOnSave) {
+        return false;
+    }
 
-    const fixAll = codeActionsOnSave !== undefined ? !!codeActionsOnSave[constants.configurationKeys.fixAll] : false;
-    const organizeImports = codeActionsOnSave !== undefined ? !!codeActionsOnSave[constants.configurationKeys.organizeImports] : false;
+    const fixAll = !!codeActionsOnSave[constants.configurationKeys.fixAll];
+    const organizeImports = !!codeActionsOnSave[constants.configurationKeys.organizeImports];
+
+    const formatOnSave = !!configuration.inspect(constants.configurationKeys.formatOnSave)!.globalValue;
 
     return formatOnSave && fixAll && organizeImports;
 }
